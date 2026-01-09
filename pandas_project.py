e3fce4a22ffd9b0f93a4cbf09003b7654160d4d5
Krize Bhattarai <cyberkrize@gmail.com>
2026-01-09 20:02:08 +1100

    pandas mini project with csv file

---
+++ pandas_project.py
@@ -0,0 +1,10 @@
+# Pandas project
+
+import pandas as pd
+
+
+
+df = pd.read_csv(r"C:\Users\Krize Bhattarai\Downloads\Python Class Recordings-20251203T095411Z-1-001\Python Class Recordings\Class 6\services.csv")
+print(df)
+
+print(df.head())
